76258adbc0ab60264249426ce75a596236921d64
AsynchronousGillz <ganvana@rams.colostate.edu>
2017-01-28 19:03:23 -0700

    test.sh use a random machine.

--- P1/test.sh
+++ P1/test.sh
@@ -1,16 +1,13 @@
 #!/usr/bin/env bash
 
-# Select nodes, info in ~info/machines
-messaging_nodes="earth jupiter mars mercury neptune saturn uranus venus topeka"
-
 # Login and kick up all messaging nodes
-for host in $messaging_nodes; do
-  tmux splitw "ssh ganvana@${host}.cs.colostate.edu"
-  tmux select-layout even-vertical
+for i in `seq 1 10`; do
+	tmux splitw "ssh $(rSSH)"
+	tmux select-layout even-vertical
 done
 
 # Makes all the terminals share the same input
 tmux set-window-option synchronize-panes on
 
 # Otherwise the last pane will still be local
-ssh ganvana@frankfort.cs.colostate.edu
+ssh $(rSSH)
